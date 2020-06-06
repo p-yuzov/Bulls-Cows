707aa4c0284916cf354db53ba8c6fc4ecd4e5d6d
Pavel Yuzov <p.yuzov@gmail.com>
2020-06-07 00:13:12 +0700

    Доработана функция newGame: 1.Выведены в консоль правила игры 2.Загадано число 3.Сделаны 2 исхода игры: выход в главное меню и выйгрыш

--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -1,6 +1,6 @@
 import Core
 import java.lang.Runtime.*
-
+val core = Core()
 fun main(){
     do {
         getRuntime().exec("clear")
@@ -23,5 +23,25 @@ fun main(){
 }
 
 fun newGame() {
-    return
+    getRuntime().exec("clear")
+    println("Загадано число 4-значное число с неповторяющимися цифрами")
+    println("Введите число, нажмите Enter")
+    println("Приложение выводит в консоль, сколько цифр угадано без совпадения")
+    println("с их позициями в тайном числе (то есть количество коров) и")
+    println("сколько угадано вплоть до позиции в тайном числе (то есть количество быков).")
+    println("")
+    println("Для выхода введите 0")
+    val secretNumber=core.getNumber()
+    do{
+        val input = readLine()
+        if (input=="0") {
+            return
+        }
+        if(input==secretNumber.toString()){
+            println("Вы выйграли")
+            println("Для продолжения нажмите Enter")
+            readLine()
+        }
+
+    }while (true)
 }
