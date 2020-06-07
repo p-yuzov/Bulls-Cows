02f277523f9d0eca9914bd2e231c02653f03adb9
Pavel Yuzov <p.yuzov@gmail.com>
2020-06-07 13:27:20 +0700

    Доработана функция newGame:     1.Добавлена проверка о корректности введеных данных     2.Добавлен подсчет коров и быков В класс Core добавлено:     1.Структура BullsCowsStruct     2.Метод checkInput     3.Метод calcBullsCows Написаны тесты

--- src/main/java/Core.kt
+++ src/main/java/Core.kt
@@ -1,9 +1,53 @@
 class Core {
+    class BullsCowsStruct {
+        var bulls: Int = 0
+        var cows: Int = 0
+    }
     fun getNumber():Int{
         var randomList = (0..9).shuffled().subList(0,4)
         while(randomList[0]==0){
             randomList = (0..9).shuffled().subList(0,4)
         }
         return randomList[0]*1000+randomList[1]*100+randomList[2]*10+randomList[3]
+    }
+
+    fun clearConsole(){
+        print("\u001b[H\u001b[2J")
+    }
+
+    fun checkInput(s: String?): Boolean {
+        if (s != null) {
+            if (s.length!=4){
+                return false
+            }
+            if (s.toIntOrNull()==null){
+                return false
+            }
+            for (i in 0..s.length-2){
+                for(j in i+1..s.length-1){
+                    if (s[i]==s[j]){
+                        return false
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
+    fun calcBullsCows(input:String,secret:String):BullsCowsStruct{
+        val result = BullsCowsStruct()
+        for(i in 0..secret.length-1){
+            for(j in 0..input.length-1){
+                if(secret[i]==input[j]){
+                    if(i==j){
+                        result.bulls++
+                    }
+                    else{
+                        result.cows++
+                    }
+                }
+            }
+        }
+        return result
     }
 }
--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -1,9 +1,9 @@
 import Core
 import java.lang.Runtime.*
 val core = Core()
-fun main(){
+fun main(args: Array<String>){
     do {
-        getRuntime().exec("clear")
+        core.clearConsole()
         println("1-Новая игра ")
         println("0-Выход ")
         val input = readLine()
@@ -23,17 +23,19 @@ fun main(){
 }
 
 fun newGame() {
-    getRuntime().exec("clear")
-    println("Загадано число 4-значное число с неповторяющимися цифрами")
-    println("Введите число, нажмите Enter")
+    core.clearConsole()
+    println("Загадано 4-значное число с неповторяющимися цифрами")
     println("Приложение выводит в консоль, сколько цифр угадано без совпадения")
     println("с их позициями в тайном числе (то есть количество коров) и")
     println("сколько угадано вплоть до позиции в тайном числе (то есть количество быков).")
     println("")
     println("Для выхода введите 0")
+    println("Для продолжения введите число, нажмите Enter")
     val secretNumber=core.getNumber()
+    var input = String()
+    var bullsCows = Core.BullsCowsStruct()
     do{
-        val input = readLine()
+        input = readLine().toString()
         if (input=="0") {
             return
         }
@@ -41,7 +43,15 @@ fun newGame() {
             println("Вы выйграли")
             println("Для продолжения нажмите Enter")
             readLine()
+            return
         }
-
+        if (! core.checkInput(input)){
+            println("Введите 4-значное число с неповторяющимися цифрами")
+        }
+        else{
+            bullsCows=core.calcBullsCows(input, secretNumber.toString())
+            println("быки: ${bullsCows.bulls}")
+            println("коровы: ${bullsCows.cows}")
+        }
     }while (true)
 }
